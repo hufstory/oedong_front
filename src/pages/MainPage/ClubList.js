b963a583bc91736344098f3e880215827674b8c1
SEUNGJUN JEONG <jsj9620@hufs.ac.kr>
2023-04-02 22:06:36 +0900

    Feat: Search Component 제작

--- src/pages/MainPage/ClubList.js
+++ src/pages/MainPage/ClubList.js
@@ -1,6 +1,10 @@
+import Search from "../../components/Search";
+
 const ClubList = () => {
   return (
-    <div className="container w-[390px] h-[514px] mx-auto flex flex-col items-center justify-center bg-main-black"></div>
+    <div className="container w-[390px] h-[514px] mx-auto flex flex-col items-center justify-center bg-main-black">
+      <Search />
+    </div>
   );
 };
 
