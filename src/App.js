ba12ba65a65b7416d227cee1929b36da1b9473a9
SEUNGJUN JEONG <jsj9620@hufs.ac.kr>
2023-04-02 16:53:52 +0900

    feat: 파일 구조 변경 / tailwind css 설치

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import "./App.css";
-
 function App() {
-  return <div className="App">a</div>;
+  return (
+    <div className="App">
+      <h1 className="bg-blue-500">hello</h1>
+    </div>
+  );
 }
 
 export default App;
