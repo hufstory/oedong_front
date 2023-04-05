ea3909fe1a03919c1c9fc2286507b48f86abb2e0
SEUNGJUN JEONG <jsj9620@hufs.ac.kr>
2023-04-05 12:40:45 +0900

    Feat: 동아리 생성 폼 시작

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
+import EnrollClub from "./pages/EnrollClub/EnrollClub";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/enroll" element={<EnrollClub />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/EnrollClub/FormHeader.js
@@ -0,0 +1,16 @@
+import logo from "../../assets/logo.png";
+
+const FormHeader = () => {
+  return (
+    <div className="container w-[390px] h-[50px] mx-auto flex flex-row items-center justify-center bg-main-black">
+      <img
+        className="object-cover h-10 inline mr-[15px]"
+        src={logo}
+        alt="logo"
+      />
+      <p className="text-white text-[32px] font-main">동아리 생성 폼</p>
+    </div>
+  );
+};
+
+export default FormHeader;
