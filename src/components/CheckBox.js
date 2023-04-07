bd731835ae6f8a1dd6c4f592e7bcf06fc74e277a
SEUNGJUN JEONG <jsj9620@hufs.ac.kr>
2023-04-07 23:29:16 +0900

    Fix: CheckBox component
    2열로 정렬 안되던 오류 수정

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -1,35 +1,35 @@
 const CheckBox = (props) => {
   const checkBoxItems = props.item.map((checkBox) => (
-    <span>
-      <input
-        type="checkbox"
-        className="form-checkbox text-green-500 rounded-full mr-[7px] relative"
-        id="check"
-        value="item"
-      />
-      <label
-        key={checkBox}
-        htmlFor="check"
-        className="text-white text-[14px] font-main mr-[20px] top-[-2.5px] relative"
-      >
-        {checkBox}
-      </label>
-    </span>
+    <div className="flex-row items-start justify-start">
+      <div className="flex-row items-start justify-start">
+        <input
+          type="checkbox"
+          className="form-checkbox text-green-500 rounded-full mr-[7px] relative"
+          id="check"
+          value="item"
+        />
+        <label
+          key={checkBox}
+          htmlFor="check"
+          className="text-white text-[14px] font-main mr-[20px] top-[-2.5px] relative"
+        >
+          {checkBox}
+        </label>
+      </div>
+    </div>
   ));
 
   const firstCol = checkBoxItems.slice(0, Math.ceil(checkBoxItems.length / 2));
   const secondCol = checkBoxItems.slice(Math.ceil(checkBoxItems.length / 2));
 
   return (
-    <div>
+    <div className="mt-[10px] mb-[10px]">
       <label className="text-white text-[16px] font-main" htmlFor="text">
         {props.label}
       </label>
-      <div className="container w-[275px] min-h-[40px] mt-[7px] pl-[29px] pt-[20px] pb-[20px] mx-auto flex flex-col items-start justify-start border-green border-2 bg-main-black">
-        <div>
-          <div className="flex-1 mb-[10px]">{firstCol}</div>
-          <div className="flex-1">{secondCol}</div>
-        </div>
+      <div className="w-[295px] min-h-[40px] mt-[7px] pl-[20px] pt-[15px] pb-[15px] flex flex-row items-start justify-center border-green border-2 bg-main-black">
+        <div className="w-1/2 mr-[12px]">{firstCol}</div>
+        <div className="w-1/2">{secondCol}</div>
       </div>
     </div>
   );
--- src/pages/EnrollClub/EnrollForm.js
+++ src/pages/EnrollClub/EnrollForm.js
@@ -4,7 +4,7 @@ import CheckBox from "../../components/CheckBox";
 
 const EnrollForm = () => {
   return (
-    <div className="container w-[390px] min-h-[100vh] mx-auto flex flex-col items-center justify-start bg-main-black">
+    <div className="container w-[390px] mx-auto flex flex-col items-center justify-start bg-main-black">
       <img
         className="w-[253px] mt-[40px] mb-[45px]"
         src={setimage}
@@ -12,9 +12,37 @@ const EnrollForm = () => {
       />
       <TextInput label="동아리 이름" />
       <CheckBox
-        label="동아리 구분"
+        label="캠퍼스 구분"
         item={["서울캠퍼스", "글로벌캠퍼스", "상관없음"]}
       />
+      <CheckBox
+        label="동아리 구분"
+        item={[
+          "일반동아리",
+          "중앙동아리",
+          "연합동아리",
+          "과 동아리",
+          "학생 자치기구",
+          "기타",
+        ]}
+      />
+      <CheckBox
+        label="동아리 분야"
+        item={[
+          "학술응용",
+          "창작예술",
+          "공연예술",
+          "개인스포츠",
+          "레저",
+          "팀스포츠",
+          "기타",
+        ]}
+      />
+      <TextInput label="동아리방 위치" />
+      <TextInput label="회장 이름" />
+      <TextInput label="회장 학번 / 학과" />
+      <TextInput label="회장 전화번호" />
+      <TextInput label="동아리 간략한 소개" />
     </div>
   );
 };
