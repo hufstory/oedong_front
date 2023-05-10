b8a5d16e853132654c8ed9880defef1c8259d830
SEUNGJUN JEONG <jsj9620@hufs.ac.kr>
2023-05-10 12:55:35 +0900

    Fix: 동아리 등록 완료 시 버튼 위치

--- src/pages/EnrollClub/EnrollSuccess.js
+++ src/pages/EnrollClub/EnrollSuccess.js
@@ -7,14 +7,10 @@ const EnrollSuccess = () => {
   return (
     <div className="container max-w-[395px] w-screen mx-auto flex flex-col items-center justify-center">
       <Header />
-      <div className="flex flex-col w-full items-center justify-center">
-        <img
-          className="relative w-[395px] bg-main-black"
-          src={success}
-          alt="success"
-        />
+      <div className="relative flex flex-col w-full items-center justify-center">
+        <img className="w-[395px] bg-main-black" src={success} alt="success" />
         <Link to="/clubinfo/admin">
-          <button className="absolute top-[535px] left-[770px] border-none hover:text-green text-white font-main text-[18px] mb-[10px]">
+          <button className="absolute top-[470px] left-[90px] border-none hover:text-green text-white font-main text-[18px] mb-[10px]">
             ▶︎ 동아리 상세페이지 가기
           </button>
         </Link>
