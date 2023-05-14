c6781e1196525407cd91a65354980da176f66f07
SEUNGJUN JEONG <jsj9620@hufs.ac.kr>
2023-05-14 12:46:34 +0900

    Feat: 버튼 누르면 사진/필터 안보이게

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/logo.png";
 import profile from "../assets/profile.png";
+import { IoTriangleSharp } from "react-icons/io5";
 
-const Header = () => {
+const Header = (props) => {
+  const location = useLocation();
+
   return (
     <div className="container mx-auto flex flex-row items-center justify-center max-w-[700px] h-20 bg-main-black">
       <Link to="/">
@@ -12,7 +15,17 @@ const Header = () => {
           alt="logo"
         />
       </Link>
-      <Link to="/enroll" className="text-white pl-[70px] text-[12px] ">
+      {location.pathname === "/" ? (
+        <p
+          className={`text-green ml-[10px] hover:cursor-pointer inline ${
+            props.showImage ? "rotate-180" : ""
+          }`}
+        >
+          <IoTriangleSharp onClick={props.handler} />
+        </p>
+      ) : null}
+
+      <Link to="/enroll" className="text-white pl-[50px] text-[12px] ">
         <span>동아리 등록</span>
       </Link>
       <Link to="/enroll" className="text-white pl-[15px] text-[12px]">
