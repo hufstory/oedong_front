70942ef4501700f9909600b8f525e603511ae9be
SEUNGJUN JEONG <jsj9620@hufs.ac.kr>
2023-05-18 23:15:48 +0900

    Style: Header 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/logo.png";
 import profile from "../assets/profile.png";
+import ShowAuth from "./ShowAuth.js";
 import { IoTriangleSharp } from "react-icons/io5";
 
 const Header = (props) => {
   const location = useLocation();
 
   return (
-    <div className="container mx-auto flex flex-row items-end justify-center max-w-[700px] h-[70px] bg-main-black">
+    <div className="container mx-auto flex flex-row items-end justify-center max-w-[700px] h-[70px] mb-[15px] bg-main-black">
       <Link to="/">
         <img
           className="object-cover h-[52px] inline ml-[10px]"
@@ -15,33 +16,35 @@ const Header = (props) => {
           alt="logo"
         />
       </Link>
-      <div className="flex flex-col items-center justify-start bg-main-black">
-        <div className="flex flex-row items-end justify-start bg-main-black">
-          {location.pathname === "/" ? (
-            <p
-              className={`text-magenta ml-[10px] hover:cursor-pointer inline ${
-                props.showImage ? "rotate-180" : ""
-              }`}
-            >
-              <IoTriangleSharp onClick={props.handler} />
-            </p>
-          ) : null}
-
-          <Link to="/enroll" className="text-white pl-[50px] text-[12px] ">
-            동아리 등록
-          </Link>
-          <Link to="/enroll" className="text-white pl-[15px] text-[12px]">
-            내 동아리
-          </Link>
-          <Link to="/enroll" className="text-white pl-[15px] text-[12px]">
-            로그아웃
-          </Link>
-          <img
-            className="object-cover h-10 inline pl-[20px]"
-            src={profile}
-            alt="profile"
-          />
+      <div className="flex flex-row items-center justify-start bg-main-black">
+        <div className="flex flex-col items-end justify-start bg-main-black">
+          <ShowAuth />
+          <div className="flex flex-row pt-[5px]">
+            {location.pathname === "/" ? (
+              <p
+                className={`text-magenta ml-[10px] hover:cursor-pointer inline ${
+                  props.showImage ? "rotate-180" : ""
+                }`}
+              >
+                <IoTriangleSharp onClick={props.handler} />
+              </p>
+            ) : null}
+            <Link to="/enroll" className="text-white pl-[50px] text-[12px] ">
+              동아리 등록
+            </Link>
+            <Link to="/enroll" className="text-white pl-[15px] text-[12px]">
+              내 동아리
+            </Link>
+            <Link to="/enroll" className="text-white pl-[15px] text-[12px]">
+              로그아웃
+            </Link>
+          </div>
         </div>
+        <img
+          className="object-cover h-10 inline pl-[20px]"
+          src={profile}
+          alt="profile"
+        />
       </div>
     </div>
   );
---
+++ src/components/ShowAuth.js
@@ -0,0 +1,14 @@
+const ShowAuth = () => {
+  return (
+    <div className="w-[88px] h-[20px] border border-green font-main text-white flex items-center">
+      <div className="w-[80px] h-[18px] text-[13px] bg-main-black flex justify-center items-center">
+        일반
+      </div>
+      <div className="w-[80px] h-[18px] text-[13px] bg-green flex justify-center items-center">
+        회장
+      </div>
+    </div>
+  );
+};
+
+export default ShowAuth;
