1635fe7a078a9253c5ce4fc162a367eaa17c40a2
SEUNGJUN JEONG <jsj9620@hufs.ac.kr>
2023-05-18 15:00:43 +0900

    Style: 동아리 상세 페이지 (운영자)

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,38 +7,42 @@ const Header = (props) => {
   const location = useLocation();
 
   return (
-    <div className="container mx-auto flex flex-row items-center justify-center max-w-[700px] h-20 bg-main-black">
+    <div className="container mx-auto flex flex-row items-end justify-center max-w-[700px] h-[70px] bg-main-black">
       <Link to="/">
         <img
-          className="object-cover h-10 inline ml-[10px]"
+          className="object-cover h-[52px] inline ml-[10px]"
           src={logo}
           alt="logo"
         />
       </Link>
-      {location.pathname === "/" ? (
-        <p
-          className={`text-green ml-[10px] hover:cursor-pointer inline ${
-            props.showImage ? "rotate-180" : ""
-          }`}
-        >
-          <IoTriangleSharp onClick={props.handler} />
-        </p>
-      ) : null}
+      <div className="flex flex-col items-center justify-start bg-main-black">
+        <div className="flex flex-row items-end justify-start bg-main-black">
+          {location.pathname === "/" ? (
+            <p
+              className={`text-magenta ml-[10px] hover:cursor-pointer inline ${
+                props.showImage ? "rotate-180" : ""
+              }`}
+            >
+              <IoTriangleSharp onClick={props.handler} />
+            </p>
+          ) : null}
 
-      <Link to="/enroll" className="text-white pl-[50px] text-[12px] ">
-        <span>동아리 등록</span>
-      </Link>
-      <Link to="/enroll" className="text-white pl-[15px] text-[12px]">
-        내 동아리
-      </Link>
-      <Link to="/enroll" className="text-white pl-[15px] text-[12px]">
-        로그아웃
-      </Link>
-      <img
-        className="object-cover h-10 inline pl-[20px]"
-        src={profile}
-        alt="profile"
-      />
+          <Link to="/enroll" className="text-white pl-[50px] text-[12px] ">
+            동아리 등록
+          </Link>
+          <Link to="/enroll" className="text-white pl-[15px] text-[12px]">
+            내 동아리
+          </Link>
+          <Link to="/enroll" className="text-white pl-[15px] text-[12px]">
+            로그아웃
+          </Link>
+          <img
+            className="object-cover h-10 inline pl-[20px]"
+            src={profile}
+            alt="profile"
+          />
+        </div>
+      </div>
     </div>
   );
 };
